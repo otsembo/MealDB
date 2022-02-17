d3081dcad797709986b7cfafc0151ffacf301332
otsembo <okumu.otsembo@gmail.com>
2022-02-17 16:06:56 +0300

    UI: Created Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./presentation/home/home.component";
+import { HeaderComponent } from './presentation/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HeaderComponent,
+    ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/presentation/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  //global variables
+  bannerImg:string = "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
+  specialsImg:string = "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80"
+  specialsTitle:string = "Chesse Burger"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
